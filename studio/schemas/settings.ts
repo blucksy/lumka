6077fe76b56cb636306ae00ca9cec6fb96c53e81
Oliver Buckley <30906123+ApalloAltoBluck@users.noreply.github.com>
2026-02-07 12:13:22 -0500

    feature: added homepage info

--- studio/schemas/settings.ts
+++ studio/schemas/settings.ts
@@ -21,79 +21,42 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
-      name: 'highlight',
-      description: 'Highlight color for links',
-      title: 'Highlight Color',
-      type: 'color',
-    }),
-    defineField({
-      name: 'thumbnailLabel',
-      description: 'Verbiage for product page (previously known as thumbnail label)',
-      title: 'Thumbnail Label',
-      type: 'string',
-    }),
-    defineField({
-      name: 'biography',
-      title: 'Biography',
-      type: 'blockContent',
-    }),
-    defineField({
-      name: 'contact',
-      title: 'Contact',
-      type: 'blockContent',
-    }),
-    defineField({
-      name: 'fellowshipHonorsAwards',
-      title: 'Fellowship, Honors & Awards',
-      type: 'blockContent',
-    }),
-    defineField({
-      name: 'selectedPublicationsClients',
-      title: 'Selected Publications & Clients',
-      type: 'blockContent',
-    }),
-    defineField({
-      name: 'selectedPress',
-      title: 'Selected Press',
-      type: 'blockContent',
-    }),
-    defineField({
-      name: 'exhibitions',
-      title: 'Exhibitions',
-      type: 'blockContent',
-    }),
-    // create object mobilesettings that has fields (contact string with url, manager rich text, prints/inquiries string, social media rich text)
-    defineField({
-      name: 'mobileSettings',
-      title: 'Mobile Settings',
-      type: 'object',
-      fields: [
-        defineField({
-          name: 'contactLink',
-          title: 'Contact Link',
-          type: 'blockContent',
-        }),
-        defineField({
-          name: 'manager',
-          title: 'Manager',
-          type: 'blockContent',
-        }),
-        defineField({
-          name: 'printsInquiries',
-          title: 'Prints/Inquiries',
-          type: 'blockContent',
-        }),
-        defineField({
-          name: 'socialMedia',
-          title: 'Social Media',
-          type: 'blockContent',
-        }),
+      name: 'publications',
+      title: 'Selected News',
+      type: 'array',
+      of: [
+        {
+          type: 'object',
+          fields: [
+            {
+              name: 'image',
+              title: 'Image',
+              type: 'image',
+            },
+            {
+              name: 'category',
+              title: 'Category',
+              description: 'ex: offsite, reviewed, announcement',
+              type: 'string',
+            },
+            {
+              name: 'title',
+              title: 'Title',
+              type: 'blockContent',
+            },
+            {
+              name: 'subtitle',
+              title: 'Subtitle',
+              type: 'blockContent',
+            },
+            {
+              name: 'link',
+              title: 'Link',
+              type: 'url',
+            },
+          ],
+        },
       ],
-      // drop down
-      options: {
-        collapsible: true,
-        collapsed: true,
-      },
     }),
   ],
   preview: {
