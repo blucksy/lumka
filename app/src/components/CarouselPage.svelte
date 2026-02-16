a4de1076211417a71128fee59a62e4ab3e0eac7b
Tomás Carlson <34401946+TomasCarlson@users.noreply.github.com>
2026-02-16 14:14:23 -0500

    feature: careosuel

--- app/src/components/CarouselPage.svelte
+++ app/src/components/CarouselPage.svelte
@@ -87,8 +87,12 @@
 			{#each items as item, i}
 				<div
 					id="slide-{i}"
-					class="swiper-slide w-fit! [&:not(.swiper-slide-active)]:*:opacity-20 [&:not(.swiper-slide-active)]:*:pointer-events-none lg:[--span:1]! py-[18px]"
+					class="swiper-slide w-fit! [&:not(.swiper-slide-active)]:cursor-pointer [&:not(.swiper-slide-active)]:hover:*:opacity-40 [&:not(.swiper-slide-active)]:*:opacity-20 [&:not(.swiper-slide-active)]:*:pointer-events-none lg:[--span:1]! py-[18px]"
 					aria-hidden={item.slug.current !== currentItem?.slug.current}
+					on:click={() => {
+						const index = items.findIndex((i) => i.slug.current === item.slug.current);
+						swiper.slideToLoop(index);
+					}}
 				>
 					<div
 						class="col-span [--span:13] md:[--span:11] lg:[--span:9] transition-opacity duration-500"
