e1f828437d9ae126e476c528cb0ca6f9c7d44b2a
Tomás Carlson <34401946+TomasCarlson@users.noreply.github.com>
2026-02-17 18:16:27 -0500

    Update +page.svelte

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -31,7 +31,7 @@
 			>
 				<ImageWrapper alt={item.title || 'LUmkA'} image={item.image} className="aspect-5/3 " />
 				<p class="small-serif small-caps">{item.category}</p>
-				<p class="mobile-link sm:link-serif col-span xl:[--span:7] mx-auto">
+				<p class="mobile-link sm:link-serif col-span xl:[--span:7] 2xl:[--span:5] mx-auto">
 					{#if item.link}
 						<TextRotate item={item.title} />
 					{:else}
