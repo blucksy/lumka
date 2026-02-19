1e790893dbacfbeaaace054a1313fdc5620ba40a
Oliver Buckley <30906123+ApalloAltoBluck@users.noreply.github.com>
2026-02-19 12:48:54 -0500

    Feature: duplicate preview return remove

--- app/src/routes/+page.server.ts
+++ app/src/routes/+page.server.ts
@@ -1,20 +1,13 @@
 import groq from 'groq';
 import type { LayoutServerLoad } from './$types';
 export const load: LayoutServerLoad = async (event) => {
-	const { preview, loadQuery } = event.locals;
+	const { loadQuery } = event.locals;
 
 	const query = groq`*[_type == "homepage" ][0] {publications[]{...}}`;
 
-	const initial = await loadQuery(
-		query,
-		{},
-		{
-			preview
-		}
-	);
+	const initial = await loadQuery(query);
 
 	return {
-		preview,
 		query,
 		options: {
 			initial
