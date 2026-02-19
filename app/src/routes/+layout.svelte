05f04e76e9beb1bc6ffd5f9cf6d2c6c47a18e5c9
Oliver Buckley <30906123+ApalloAltoBluck@users.noreply.github.com>
2026-02-19 12:53:50 -0500

    Feature: live preview overlay

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -33,7 +33,7 @@
 			.replace(/\/artists\/[^/]+/, '/artists/*');
 </script>
 
-<!-- {#if $isPreviewing}
+{#if $isPreviewing}
 	<a
 		href={`/preview/disable?redirect=${$page.url.pathname}`}
 		class="inline-flex opacity-50 z-50 bottom-0 left-0 h-min fixed bg-black/10 px-3 items-center gap-2 text-sm text-neutral-600 hover:text-neutral-900 underline-offset-4 hover:underline transition"
@@ -41,7 +41,7 @@
 		<span>Preview enabled</span>
 		<span class="opacity-60">Disable</span>
 	</a>
-{/if} -->
+{/if}
 
 <Nav data={siteData} />
 
