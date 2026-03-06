5ea7a4691da4fd3793a11f1b90d6fbb7afaa76ce
Tomás Carlson <34401946+TomasCarlson@users.noreply.github.com>
2026-03-06 15:34:00 -0500

    feature: 404

---
+++ app/src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<div class="min-h-screen flex w-full px-page items-center justify-center flex-col gap-[24px]">
+	<h1 class="body">Page Not Found</h1>
+	<div class="flex items-center">
+		<a href="/" class="hover:opacity-60 anim-opacity w-fit mx-auto sans cursor-pointer"
+			>Return to Home</a
+		>
+	</div>
+</div>
